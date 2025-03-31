d648e5fc18c6a14d2eeb3bfb34ece77221e244f3
Ben Coleman <benc.uk@gmail.com>
2025-03-31 18:23:30 +0100

    Update Vite configuration: set base path and server port; remove unused vite.config.mjs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/nanotracker2/',
   build: {
     target: 'esnext', // Enables Top-level await
+  },
+  server: {
+    port: 3000,
   },
 })
